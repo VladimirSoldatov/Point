063596ddc290311f0816221151084805213d9770
Владимир Солдатов <cbb110@rambler.ru>
2025-03-19 16:41:40 +0300

    fix_minor

--- WinFormsApp3/Form1.Designer.cs
+++ WinFormsApp3/Form1.Designer.cs
@@ -36,11 +36,13 @@
             button5 = new Button();
             progressBar1 = new ProgressBar();
             label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(45, 391);
+            button1.Location = new Point(27, 368);
             button1.Name = "button1";
             button1.Size = new Size(119, 23);
             button1.TabIndex = 0;
@@ -51,7 +53,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(45, 417);
+            label1.Location = new Point(27, 340);
             label1.Name = "label1";
             label1.Size = new Size(38, 15);
             label1.TabIndex = 1;
@@ -59,7 +61,7 @@
             // 
             // button2
             // 
-            button2.Location = new Point(200, 391);
+            button2.Location = new Point(182, 368);
             button2.Name = "button2";
             button2.Size = new Size(140, 23);
             button2.TabIndex = 2;
@@ -69,7 +71,7 @@
             // 
             // button3
             // 
-            button3.Location = new Point(357, 391);
+            button3.Location = new Point(339, 368);
             button3.Name = "button3";
             button3.Size = new Size(124, 23);
             button3.TabIndex = 3;
@@ -79,7 +81,7 @@
             // 
             // button4
             // 
-            button4.Location = new Point(497, 391);
+            button4.Location = new Point(479, 368);
             button4.Name = "button4";
             button4.Size = new Size(128, 23);
             button4.TabIndex = 4;
@@ -89,7 +91,7 @@
             // 
             // button5
             // 
-            button5.Location = new Point(648, 391);
+            button5.Location = new Point(630, 368);
             button5.Name = "button5";
             button5.Size = new Size(140, 23);
             button5.TabIndex = 5;
@@ -99,7 +101,7 @@
             // 
             // progressBar1
             // 
-            progressBar1.Location = new Point(372, 420);
+            progressBar1.Location = new Point(354, 397);
             progressBar1.Name = "progressBar1";
             progressBar1.Size = new Size(276, 23);
             progressBar1.TabIndex = 6;
@@ -107,17 +109,37 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(679, 420);
+            label2.Location = new Point(710, 405);
             label2.Name = "label2";
             label2.Size = new Size(38, 15);
             label2.TabIndex = 7;
             label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(641, 406);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 8;
+            label3.Text = "label3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(685, 406);
+            label4.Name = "label4";
+            label4.Size = new Size(19, 15);
+            label4.TabIndex = 9;
+            label4.Text = "из";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label4);
+            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(progressBar1);
             Controls.Add(button5);
@@ -142,5 +164,7 @@
         private Button button5;
         private ProgressBar progressBar1;
         private Label label2;
+        private Label label3;
+        private Label label4;
     }
 }
--- WinFormsApp3/Form1.cs
+++ WinFormsApp3/Form1.cs
@@ -8,6 +8,13 @@ namespace WinFormsApp3
         {
             InitializeComponent();
 
+            resetEvent = new ManualResetEvent(false);
+
+
+        }
+        CancellationTokenSource source;
+        CancellationToken token;
+        List<Task> tasks = new List<Task>();
         private void button1_Click(object sender, EventArgs e)
         {
             progressBar1.Minimum = 0;
@@ -20,6 +27,104 @@ namespace WinFormsApp3
             {
                 var list = form2.list_of_words;
             }
+
+            Potok_1();
+            Potok_2();
+            Potok_3();
+            tasks.ForEach(p => p.Start());
+
+
+        }
+        ManualResetEvent resetEvent;
+
+
+        private void Potok_1()
+        {
+            Task task1 = new Task(Searcher, token);
+
+            tasks.Add(task1);
+
+            resetEvent.Set();
+        }
+        private void Potok_2()
+        {
+            Task task2 = new Task(
+                () =>   {
+                            int count = Count_of_files();
+                            Invoke(GetMaximum, count);
+                        });
+            tasks.Add(task2);
+        }
+
+        int Count_of_files()
+        {
+            FileSearcher fs = new FileSearcher();
+            IEnumerable<string> objects = fs.GetFiles3("c:\\", "*.txt");
+            int count = objects.Count();
+            return count;
+        }
+
+        private void Potok_3()
+        {
+
+
+            Task task3 = new Task(() =>
+            {
+                while (true)
+                {
+                    int maximum = progressBar1.Maximum;
+                    Invoke(() => 
+                                {
+                                    label2.Text = maximum.ToString();
+                                });
+                    Thread.Sleep(10000);
+                }
+            });
+            tasks.Add(task3);
+
+        }
+        private void GetMaximum(int id)
+        {
+            progressBar1.Maximum = id;
+        }
+
+        private void Searcher()
+        {
+
+            FileSearcher fs = new FileSearcher();
+            foreach (string file in fs.GetFiles("c:\\", "*.txt"))
+            {
+                if (token.IsCancellationRequested)
+                    return;
+                resetEvent.WaitOne();
+                Invoke(() =>
+                {
+                    label1.Text = file;
+                    progressBar1.PerformStep();
+                });
+                Thread.Sleep(3000);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            resetEvent.Reset();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            resetEvent.Set();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            source.Cancel();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show($"{Process.GetCurrentProcess().Threads.Count} - {tasks[0].Status} - {tasks[1].Status}");
         }
     }
 }
