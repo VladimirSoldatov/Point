48d27d636d20ad2010d1a1dbe6e4812329f97fb2
Владимир Солдатов <cbb110@rambler.ru>
2025-03-20 13:15:59 +0300

    ext

--- WinFormsApp3/FileWorker.cs
+++ WinFormsApp3/FileWorker.cs
@@ -9,36 +9,10 @@ namespace WinFormsApp3
 {
     internal class FileWorker
     {
-        Dictionary<string, int> magic_words;
-
-        public FileWorker() 
+        public FileWorker() { }
+        public void SearchRegex()
         {
-            magic_words = new Dictionary<string, int>();
-        }
-         public void Insert_words(IEnumerable <string> words)
-        {
-            
-            foreach (string word in words)
-            {
-                magic_words[word] = 0;
-            }
 
-        }
-
-
-        public string ReadFile(string path)
-        {
-            string text = string.Empty;
-            using(StreamReader sr = new StreamReader(path))
-            {
-                if(sr is not null)
-                text = sr.ReadToEnd();
-            }
-            return text;
-        }
-        public int SearchRegex(string text, string magic_word)
-        {
-            return Regex.Count(text, magic_word);
         }
 
     }
